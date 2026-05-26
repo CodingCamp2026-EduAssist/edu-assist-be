2f1ee45e3ee45e6e1f53f035c7c335d60e87a409
Rayhan Atmadja <workspace.rayhanatmadja@gmail.com>
2026-05-26 14:07:51 +0700

    feat(cors): add cors

--- src/middlewares/security.middleware.ts
+++ src/middlewares/security.middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { env } from '../config/env';
 
+const allowedOrigins = [env.clientUrl];
+
 export function securityHeaders(_req: Request, res: Response, next: NextFunction): void {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
@@ -14,3 +16,19 @@ export function securityHeaders(_req: Request, res: Response, next: NextFunction
 
   next();
 }
+
+export const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    // eslint-disable-next-line no-unused-vars
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
